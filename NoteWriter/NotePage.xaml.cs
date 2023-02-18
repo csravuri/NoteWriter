57b890f2b4a471726091cd7602b82a9fc12f3871
Chandra Sekhar Ravuri <Chandra.Ravuri@wisetechglobal.com>
2023-02-18 21:48:27 +0530

    notes binding and UI

--- NoteWriter/NotePage.xaml.cs
+++ NoteWriter/NotePage.xaml.cs
@@ -4,14 +4,27 @@ namespace NoteWriter;
 
 public partial class NotePage : ContentPage
 {
-	readonly DataSource.NotesList nodeList;
-	public NotePage(NotesList nodeList) : this()
+	readonly NotesList nodeList;
+	readonly Note note;
+
+	public NotePage(NotesList nodeList, Note note = null) : this()
 	{
 		this.nodeList = nodeList;
+		this.note = note ?? new Note();
+		BindingContext = this.note;
 	}
 
 	public NotePage()
 	{
 		InitializeComponent();
+	}
+
+	private void SaveButton_Clicked(object sender, EventArgs e)
+	{
+		if (!nodeList.Exists(x => x.Id == note.Id))
+		{
+			nodeList.Add(note);
+		}
+		nodeList.Save();
 	}
 }
