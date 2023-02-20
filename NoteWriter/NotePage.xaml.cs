852063e7730a97e4726fa11f12183846d3495288
Chandra Sekhar Ravuri <Chandra.Ravuri@wisetechglobal.com>
2023-02-20 20:06:15 +0530

    image alignment and delete actual image

--- NoteWriter/NotePage.xaml.cs
+++ NoteWriter/NotePage.xaml.cs
@@ -79,6 +79,7 @@ public partial class NotePage : ContentPage
 			if (!string.IsNullOrEmpty(selectedImage))
 			{
 				note.ImagePaths.Remove(selectedImage);
+				File.Delete(selectedImage);
 			}
 		}
 	}
