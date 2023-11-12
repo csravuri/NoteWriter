f611bf164b0cd6f0ffb5f225ccd275a9f4dc7dc6
Chandra Sekhar Ravuri <35503656+csravuri@users.noreply.github.com>
2023-11-12 16:34:26 +0530

    prompt user before delete

--- NoteWriter/MainPage.xaml.cs
+++ NoteWriter/MainPage.xaml.cs
@@ -2,53 +2,58 @@
 
 public partial class MainPage : ContentPage
 {
-    readonly NotesList noteList;
-    public MainPage()
-    {
-        InitializeComponent();
-        noteList = NotesList.Load();
-        BindingContext = noteList;
-    }
+	readonly NotesList noteList;
+	public MainPage()
+	{
+		InitializeComponent();
+		noteList = NotesList.Load();
+		BindingContext = noteList;
+	}
 
-    private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
-    {
-        if (sender is ListView listView)
-        {
-            await Navigation.PushAsync(new NotePage(noteList, e.Item as Note));
-            listView.SelectedItem = null;
-        }
-    }
+	private async void ListView_ItemTapped(object sender, ItemTappedEventArgs e)
+	{
+		if (sender is ListView listView)
+		{
+			await Navigation.PushAsync(new NotePage(noteList, e.Item as Note));
+			listView.SelectedItem = null;
+		}
+	}
 
-    private async void Button_Clicked(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new NotePage(noteList));
-    }
+	private async void Button_Clicked(object sender, EventArgs e)
+	{
+		await Navigation.PushAsync(new NotePage(noteList));
+	}
 
-    private void DeleteButton_Clicked(object sender, EventArgs e)
-    {
-        if (sender is Button deleteButton && deleteButton.CommandParameter is Note note)
-        {
-            foreach (var image in note.ImagePaths)
-            {
-                if (!string.IsNullOrEmpty(image) && File.Exists(image))
-                {
-                    File.Delete(image);
-                }
-            }
+	private async void DeleteButton_Clicked(object sender, EventArgs e)
+	{
+		if (sender is Button deleteButton
+			&& deleteButton.CommandParameter is Note note
+			&& await Shell.Current.DisplayAlert("Delete!", "Want to delete?", Yes, No))
+		{
+			foreach (var image in note.ImagePaths)
+			{
+				if (!string.IsNullOrEmpty(image) && File.Exists(image))
+				{
+					File.Delete(image);
+				}
+			}
 
-            var notes = noteList.ToArray();
+			var notes = noteList.ToArray();
 
-            noteList.Clear();
-            foreach (var item in notes)
-            {
-                if (item.Id != note.Id)
-                {
-                    noteList.Add(item);
-                }
-            }
+			noteList.Clear();
+			foreach (var item in notes)
+			{
+				if (item.Id != note.Id)
+				{
+					noteList.Add(item);
+				}
+			}
 
-            noteList.Save();
-        }
-    }
+			noteList.Save();
+		}
+	}
+
+	const string Yes = "Yes";
+	const string No = "No";
 }
 
